eb461c36c33803155b442f4e4c41b2c3d0d1638a
Shayal Vaghasiya <shayalvaghasiya@gmail.com>
2025-12-18 19:39:12 +0530

    Add transaction model

--- finance/models.py
+++ finance/models.py
@@ -12,6 +12,7 @@ class AccountType(models.TextChoices):
     WALLET = "wallet", "Wallet"
 
 class Account(models.Model):
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="accounts")
     name = models.CharField(max_length=100)
     account_type = models.CharField(max_length=20, choices=AccountType.choices)
@@ -34,6 +35,7 @@ class CategoryType(models.TextChoices):
     EXPENSE = "expense", "Expense"
 
 class Category(models.Model):
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="categories")
     name = models.CharField(max_length=100)
     category_type = models.CharField(max_length=10, choices=CategoryType.choices)
@@ -45,3 +47,35 @@ class Category(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.category_type})"
+    
+
+
+# Transaction
+
+class TransactionType(models.TextChoices):
+    INCOME = "income", "Income"
+    EXPENSE = "expense", "Expense"
+
+class Transaction(models.Model):
+    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="transactions")
+    account = models.ForeignKey(Account, on_delete=models.PROTECT, related_name="transactions")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name="transactions")
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    transaction_type = models.CharField(max_length=10, choices=TransactionType.choices)
+    occured_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    note = models.TextField(blank=True)
+
+    def clean(self):
+        if self.amount <=0:
+            raise ValueError("Transaction amount must be greater than zero")
+        
+        if self.transaction_type != self.category.category_type:
+            raise ValueError("Transaction type must match the category type")
+    
+    def __str__(self):
+        return f"{self.transaction_type} - {self.amount}"
+    
+    
+        
