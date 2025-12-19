399de7716d637f101799cf80f30ce0bad8f77a40
Shayal Vaghasiya <shayalvaghasiya@gmail.com>
2025-12-19 21:38:13 +0530

    configure django admin for finance

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Account, Category, Transaction, Budget
 
-# Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("name", "account_type", "opening_balance", "user", "created_at")
+    list_filter = ("account_type", )
+    search_fields = ("name", )
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "category_type", "user", "created_at")
+    list_filter = ("category_type", )
+    search_fields = ("name", )
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("transaction_type", "amount", "category", "account", "user", "occured_at",)
+    list_filter = ("transaction_type", "category", "account", )
+    search_fields = ("note", )
+    date_hierarchy = "occured_at"
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ("category", "amount", "period", "start_date", "user",)
+    list_filter = ("period", )
+    
